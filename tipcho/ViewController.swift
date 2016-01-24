13a9612d9d5aaa6370037f7724daa0aa7fcd0e3e
dnedkova <dnedkova@lyft.com>
2016-01-23 17:30:20 -0800

    CodePath tip calculator

---
+++ tipcho/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  tipcho
+//
+//  Created by Diana Nedkova on 1/22/16.
+//  Copyright © 2016 Diana Nedkova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        let tipPercentages = [0.18, 0.2, 0.22]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+
+    @IBAction func onTap(sender: AnyObject) {
+        
+        view.endEditing(true)
+    }
+}
+
